fa7f3bdecbc7777522d4d804bff59a8de2ae543b
Rami Krispin <ramkrisp@umich.edu>
2017-12-16 09:22:34 -0500

    Updating the acf and pacf functions

--- acf_ly.R
+++ acf_ly.R
@@ -1,9 +1,9 @@
 acf_ly <- function(ts.obj, lag.max = NULL, ci = 0.95){
   `%>%` <- magrittr::`%>%`
-  
+  # Error handling
   if(is.null(ts.obj)){
     stop("The time series object is NULL")
-  } else if(!is.ts(ts.obj) & !xts::is.xts(ts.obj) & !zoo::is.zoo(ts.obj)){
+  } else if(!stats::is.ts(ts.obj) & !xts::is.xts(ts.obj) & !zoo::is.zoo(ts.obj)){
     stop('Invalid class - Please make sure the object class is either "ts", "mts", "xts" or "zoo"')
   }
   
@@ -14,31 +14,33 @@ acf_ly <- function(ts.obj, lag.max = NULL, ci = 0.95){
   
   x <- df <- obj.name <- NULL
   
-  obj.name <- deparse(substitute(ts.obj))
-  x <- acf(ts.obj, lag.max = lag.max, plot = FALSE)
+  obj.name <- base::deparse(base::substitute(ts.obj))
+  x <- stats::acf(ts.obj, lag.max = lag.max, plot = FALSE)
   
-  
+  # Error handling
   if(!is.null(lag.max)){
     if(lag.max > x[[3]]){
       warning(paste("The value of 'lag.max' exceed the number of available lags in the series, 
                     by default will use the max available lags", sep = " "))
     }
   }
+  # Calculate the upper/lower CI values
+  ci_value <- stats::qnorm((1 + ci)/2)/sqrt(x[[3]])
   
-  ci_value <- qnorm((1 + ci)/2)/sqrt(x[[3]])
-  
-  df <- data.frame(lag = round(x[[4]], 3), 
-                   acf = round(x[[1]], 3), 
+  # Convert the values into data frame format
+  df <- data.frame(lag = base::round(x[[4]], 3), 
+                   acf = base::round(x[[1]], 3), 
                    width = 0.02, 
-                   ci_u = round(ci_value,2), 
-                   ci_d = round(-ci_value,2)
+                   ci_u = base::round(ci_value,2), 
+                   ci_d = base::round(-ci_value,2)
                    )
+  # Multivariate time series
   series_list <- series <- NULL
     if(ncol(df) > 5){
       if(!is.null(x[[6]])){
         series <- x[[6]]
       } else{
-        for(i in 1:(dim(ts.obj)[2])){
+        for(i in 1:(base::dim(ts.obj)[2])){
           series <- c(series, paste("Series", i, sep = " "))
         }
       }
@@ -48,11 +50,16 @@ acf_ly <- function(ts.obj, lag.max = NULL, ci = 0.95){
           counter <- counter + 1
           if(r == c){
             series_list[counter] <- plotly::plot_ly() %>% 
-              plotly::add_bars(x = df[, counter], y = df[,(length(series))^2 + counter ], 
+              plotly::add_bars(x = df[, counter], 
+                               y = df[,(length(series))^2 + counter ], 
                                width = df$width, name = "ACF") %>%
-              plotly::add_trace(x = df[, counter], y = df$ci_u, type = "scatter", mode = "lines", 
+              plotly::add_trace(x = df[, counter], 
+                                y = df$ci_u, 
+                                type = "scatter", mode = "lines", name = "CI Upper Bound",  
                                 line = list(width = 1, dash = "dash", color = "green")) %>%
-              plotly::add_trace(x = df[, counter], y = df$ci_d, type = "scatter", mode = "lines", 
+              plotly::add_trace(x = df[, counter], 
+                                y = df$ci_d, 
+                                type = "scatter", mode = "lines", name = "CI Upper Bound",   
                                 line = list(width = 1, dash = "dash", color = "green")) %>%
               plotly::layout(
                 xaxis = list(title = "Lag", showgrid = FALSE),
@@ -65,17 +72,22 @@ acf_ly <- function(ts.obj, lag.max = NULL, ci = 0.95){
                   xanchor = "center",
                   align = "center",
                   x = 0.5,
-                  y = 1,
+                  y = 0.9,
                   showarrow = FALSE
                 )
               )
           } else {
             series_list[counter] <- plotly::plot_ly() %>% 
-              plotly::add_bars(x = df[, counter], y = df[,(length(series))^2 + counter ], 
+              plotly::add_bars(x = df[, counter], 
+                               y = df[,(length(series))^2 + counter ], 
                                width = df$width, name = "ACF") %>%
-              plotly::add_trace(x = df[, counter], y = df$ci_u, type = "scatter", mode = "lines", 
+              plotly::add_trace(x = df[, counter], 
+                                y = df$ci_u, 
+                                type = "scatter", mode = "lines", name = "CI Upper Bound",   
                                 line = list(width = 1, dash = "dash", color = "green")) %>%
-              plotly::add_trace(x = df[, counter], y = df$ci_d, type = "scatter", mode = "lines", 
+              plotly::add_trace(x = df[, counter], 
+                                y = df$ci_d, 
+                                type = "scatter", mode = "lines", name = "CI Lower Bound",   
                                 line = list(width = 1, dash = "dash", color = "green")) %>%
               plotly::layout(
                 xaxis = list(title = "Lag", showgrid = FALSE),
@@ -96,61 +108,32 @@ acf_ly <- function(ts.obj, lag.max = NULL, ci = 0.95){
         }
       }
       p <- plotly::subplot(series_list, nrows = length(series), 
-                           titleX = TRUE, titleY = TRUE, margin = 0.03,
+                           titleX = FALSE, titleY = TRUE, margin = 0.03,
                            shareX = FALSE, shareY = TRUE) %>% 
         plotly::hide_legend() %>% 
-        plotly::layout(title = "ACF", margin = 0.06)
+        plotly::layout(title = "ACF Plot", margin = 0.06)
+      } else if(ncol(df) == 5){
+        p <- plotly::plot_ly(data = df) %>% 
+          plotly::add_bars(x = ~lag, 
+                           y = ~acf, 
+                           width = ~ width, 
+                           name = "ACF") %>%
+          plotly::add_trace(x = ~ lag, 
+                            y = ~ci_u, 
+                            type = "scatter", mode = "lines", name = "CI Upper Bound",   
+                            line = list(width = 1, dash = "dash", color = "green")) %>%
+          plotly::add_trace(x = ~ lag, 
+                            y = ~ci_d, 
+                            type = "scatter", mode = "lines", name = "CI Lower Bound",   
+                            line = list(width = 1, dash = "dash", color = "green")) %>%
+          plotly::layout(
+            title = paste("Series", obj.name, sep = " "),
+            xaxis = list(title = "Lag", showgrid = FALSE),
+            yaxis = list(title = "ACF", showgrid = FALSE)
+          ) %>%
+          plotly::hide_legend()
       }
-    
-  p
-  
-  
-  ci_value <- qnorm((1 + ci)/2)/sqrt(x[[3]])
-  df <- data.frame(lag = round(x[[4]], 3), 
-                   acf = round(x[[1]], 3), 
-                   width = 0.02, 
-                   ci_u = round(ci_value,2), 
-                   ci_d = round(-ci_value,2)
-                   )
-
-  p <- plotly::plot_ly(data = df) %>% 
-    plotly::add_bars(x = ~lag, y = ~acf, width = ~ width, name = "ACF") %>%
-    plotly::add_trace(x = ~ lag, y = ~ci_u, type = "scatter", mode = "lines", 
-                      line = list(width = 1, dash = "dash", color = "green")) %>%
-    plotly::add_trace(x = ~ lag, y = ~ci_d, type = "scatter", mode = "lines", 
-                      line = list(width = 1, dash = "dash", color = "green")) %>%
-    plotly::layout(
-      title = paste("Series", obj.name, sep = " "),
-      xaxis = list(title = "Lag", showgrid = FALSE),
-      yaxis = list(title = "ACF", showgrid = FALSE)
-    ) %>%
-    plotly::hide_legend()
-  
   
   return(p)
 }
 
-pacf_ly <- function(ts.obj, lag.max = NULL, ci = 0.95){
-  `%>%` <- magrittr::`%>%`
-  x <- df <- obj.name <- NULL
-  obj.name <- deparse(substitute(ts.obj))
-  x <- pacf(ts.obj, lag.max = lag.max, plot = FALSE)
-  ci_value <- qnorm((1 + ci)/2)/sqrt(x[[3]])
-  df <- data.frame(lag = x[[4]], acf = x[[1]], width = 0.02, ci_u = ci_value, ci_d = -ci_value)
-  
-  p <- plotly::plot_ly(data = df) %>% 
-    plotly::add_bars(x = ~lag, y = ~acf, width = ~ width) %>%
-    plotly::add_trace(x = ~ lag, y = ~ci_u, type = "scatter", mode = "lines", 
-                      line = list(width = 1, dash = "dash", color = "green")) %>%
-    plotly::add_trace(x = ~ lag, y = ~ci_d, type = "scatter", mode = "lines", 
-                      line = list(width = 1, dash = "dash", color = "green")) %>%
-    plotly::layout(
-      title = paste("Series", obj.name, sep = " "),
-      xaxis = list(title = "Lag", showgrid = FALSE),
-      yaxis = list(title = "ACF", showgrid = FALSE)
-    ) %>%
-    plotly::hide_legend()
-  
-  
-  return(p)
-}
