1f2819ebdb3f9e80f2d0d68192895464f2cc02f6
Rami Krispin <ramkrisp@umich.edu>
2017-12-17 00:47:45 -0500

    Visualization function for the fitted and forecasted values

---
+++ fortest_ly.R
@@ -0,0 +1,49 @@
+fortest_ly <- function(actual, forecast.obj, train, test, Ygrid = FALSE, Xgrid = FALSE){
+  # Error handling
+  if(!forecast::is.forecast(forecast.obj)){
+    stop('The class of theforecast object is not "forecast"')
+  } 
+  if(length(train) + length(test) != length(actual)){
+    stop("The length of the train and test sets are different from the length of the actual set")
+  } 
+  if(!base::is.logical(Ygrid)){
+    warning('The value of "Ygrid" is not boolean, using the default option (FALSE)')
+    Ygrid <- FALSE
+  }
+  
+  if(!base::is.logical(Xgrid)){
+    warning('The value of "Xgrid" is not boolean, using the default option (FALSE)')
+    Xgrid <- FALSE
+  }
+  time_actual <- obj.name <- NULL
+  
+  obj.name <- base::deparse(base::substitute(actual))
+  
+  if(stats::is.ts(actual)){
+    time_actual <- stats::time(actual)
+  } else if(zoo::is.zoo(actual) | xts::is.xts(actual)){
+    time_actual <- zoo::index(actual)
+  }
+  
+  p <- plotly::plot_ly() %>% 
+    
+  plotly::add_trace(x = time_actual, 
+            y = as.numeric(actual), 
+            mode = 'lines+markers', name = "Actual", type = 'scatter') %>%  
+  plotly::add_trace(x = time_actual, 
+            y = c(forecast.obj$fitted, rep(NA,length(actual) - length(train))), 
+            mode = 'lines+markers',name = "Fitted", type = 'scatter',
+            line = list(color = "red")) %>%
+  plotly::add_trace(x = time_actual, 
+            y = c(rep(NA,length(actual) - length(test)), forecast.obj$mean), 
+            mode = 'lines+markers',name = "Forecast",type = 'scatter') %>% 
+  plotly::layout(
+    title = "Actual vs Forecast and Fitted", 
+    xaxis = list(title = forecast.obj$method, showgrid = Xgrid), 
+    yaxis = list(title = obj.name, showgrid = Ygrid)
+  )
+
+return(p)
+}
+
+
