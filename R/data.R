4ab6aa134949c66ed972e471a8a3136f426afb05
Rami Krispin <ramkrisp@umich.edu>
2018-01-07 05:21:04 -0500

    Update the package data sets

--- R/data.R
+++ R/data.R
@@ -1,6 +1,36 @@
+#' US Monthly Total Vehicle Sales
+#' 
+#' US monthly total vehicle sales: 1976 - 2017.
+#' 
+#' 
+#' @format Time series data
+#' @source U.S. Bureau of Economic Analysis, Total Vehicle Sales [TOTALNSA], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/TOTALNSA, January 7, 2018.
+#' @keywords datasets
+#' @examples
+#' ts.plot_ly(USVSales)
+#' seasonal_ly(USVSales)
+
+"USVSales"
+
+#' US Monthly Civilian Unemployment Rate
+#' 
+#' US monthly civilian unemployment rate: 1948 - 2017.
+#' 
+#' 
+#' @format Time series data
+#' @source U.S. Bureau of Labor Statistics, Civilian Unemployment Rate [UNRATENSA], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/UNRATENSA, January 6, 2018.
+#' @keywords datasets
+#' @examples
+#' ts.plot_ly(USUnRate)
+#' seasonal_ly(USUnRate)
+
+"USUnRate"
+
+
+
 #' US monthly natural gas consumption
 #' 
-#' US monthly natural gas consumption: 2000--2017.
+#' US monthly natural gas consumption: 2000 - 2017.
 #' 
 #' 
 #' @format Time series data
--- data-raw/data refresh.R
+++ data-raw/data refresh.R
@@ -1,5 +1,13 @@
 # Update data sets
 
+#Total US Vehicle Sales
+USVSales <- Quandl::Quandl("FRED/TOTALNSA", collapse="monthly", type = "ts") 
+devtools::use_data(USVSales)
+
+# US Monthly Unemployment Rate
+USUnRate <- Quandl::Quandl("FRED/UNRATENSA", collapse="monthly", type = "ts")
+devtools::use_data(USUnRate)
+
 # US Natural Gas Consumption
-gas <- Quandl::Quandl("FRED/NATURALGAS", collapse="monthly", type = "ts")
-devtools::use_data(gas)
+USgas <- Quandl::Quandl("FRED/NATURALGAS", collapse="monthly", type = "ts")
+devtools::use_data(USgas)
