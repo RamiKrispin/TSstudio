0aff06e29f723fafa4d73a9ce4aca3db87eae05f
Rami Krispin <ramkrisp@umich.edu>
2018-01-07 04:57:00 -0500

    Updating the US Natural Gas Consumption data set

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' US monthly natural gas consumption
+#' 
+#' US monthly natural gas consumption: 2000--2017.
+#' 
+#' 
+#' @format Time series data
+#' @source U.S. Bureau of Transportation Statistics, Natural Gas Consumption [NATURALGAS], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/NATURALGAS, January 7, 2018.
+#' @keywords datasets
+#' @examples
+#' ts.plot_ly(USgas)
+#' seasonal_ly(USgas)
+
+"USgas"
