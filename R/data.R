5eaba2f56d0e2477346a2ea34e0a44d990b49354
Rami Krispin <ramkrisp@umich.edu>
2018-01-10 20:39:50 -0500

    Update new datasets

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' US monthly total vehicle sales: 1976 - 2017.
 #' 
 #' 
-#' @format Time series data
+#' @format Time series data - 'ts' object
 #' @source U.S. Bureau of Economic Analysis, Total Vehicle Sales [TOTALNSA], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/TOTALNSA, January 7, 2018.
 #' @keywords datasets
 #' @examples
@@ -17,7 +17,7 @@
 #' US monthly civilian unemployment rate: 1948 - 2017.
 #' 
 #' 
-#' @format Time series data
+#' @format Time series data - 'ts' object
 #' @source U.S. Bureau of Labor Statistics, Civilian Unemployment Rate [UNRATENSA], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/UNRATENSA, January 6, 2018.
 #' @keywords datasets
 #' @examples
@@ -33,7 +33,7 @@
 #' US monthly natural gas consumption: 2000 - 2017.
 #' 
 #' 
-#' @format Time series data
+#' @format Time series data - 'ts' object
 #' @source U.S. Bureau of Transportation Statistics, Natural Gas Consumption [NATURALGAS], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/NATURALGAS, January 7, 2018.
 #' @keywords datasets
 #' @examples
@@ -41,3 +41,32 @@
 #' seasonal_ly(USgas)
 
 "USgas"
+
+#' University of Michigan Consumer Survey, Index of Consumer Sentiment
+#' 
+#' University of Michigan Consumer Survey, Index of Consumer Sentiment: 1980 - 2017.
+#' 
+#' 
+#' @format Time series data - 'xts' object
+#' @source University of Michigan, University of Michigan: Consumer Sentiment
+#' @keywords datasets
+#' @examples
+#' ts.plot_ly(Michigan_CS)
+#' seasonal_ly(Michigan_CS)
+
+"Michigan_CS"
+
+
+#' Crude Oil Prices: Brent - Europe
+#' 
+#' Crude Oil Prices: Brent - Europe: 1987 - 2017.
+#' 
+#' 
+#' @format Time series data - 'zoo'
+#' @source U.S. Energy Information Administration, Crude Oil Prices: Brent - Europe [MCOILBRENTEU], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/MCOILBRENTEU, January 8, 2018.
+#' @keywords datasets
+#' @examples
+#' ts.plot_ly(EURO_Brent)
+#' seasonal_ly(EURO_Brent)
+
+"EURO_Brent"
--- data-raw/data refresh.R
+++ data-raw/data refresh.R
@@ -11,3 +11,11 @@ devtools::use_data(USUnRate)
 # US Natural Gas Consumption
 USgas <- Quandl::Quandl("FRED/NATURALGAS", collapse="monthly", type = "ts")
 devtools::use_data(USgas)
+
+# University of Michigan Consumer Survey, Index of Consumer Sentiment
+Michigan_CS <- Quandl::Quandl("UMICH/SOC1", collapse="monthly", start_date="1980-01-01", type = "xts")
+devtools::use_data(Michigan_CS, overwrite = TRUE)
+
+# Crude Oil Prices: Brent - Europe
+EURO_Brent <- Quandl::Quandl("FRED/MCOILBRENTEU", collapse="monthly", type = "zoo")
+devtools::use_data(EURO_Brent, overwrite = TRUE)
