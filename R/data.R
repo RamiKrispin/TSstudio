c9cb6ba3730e5161ff1c0bbbf56969988a4c8315
RamiKrispin <ramkrisp@umich.edu>
2018-02-06 15:33:29 -0500

    Updated the units of the datasets

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' US Monthly Total Vehicle Sales
 #' 
 #' US monthly total vehicle sales: 1976 - 2017.
+#' Units: Thousands of units
 #' 
 #' 
 #' @format Time series data - 'ts' object
@@ -15,7 +16,7 @@
 #' US Monthly Civilian Unemployment Rate
 #' 
 #' US monthly civilian unemployment rate: 1948 - 2017.
-#' 
+#' Units: Percent
 #' 
 #' @format Time series data - 'ts' object
 #' @source U.S. Bureau of Labor Statistics, Civilian Unemployment Rate [UNRATENSA], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/UNRATENSA, January 6, 2018.
@@ -31,7 +32,7 @@
 #' US monthly natural gas consumption
 #' 
 #' US monthly natural gas consumption: 2000 - 2017.
-#' 
+#' Units: Billion Cubic Feet
 #' 
 #' @format Time series data - 'ts' object
 #' @source U.S. Bureau of Transportation Statistics, Natural Gas Consumption [NATURALGAS], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/NATURALGAS, January 7, 2018.
@@ -45,7 +46,7 @@
 #' University of Michigan Consumer Survey, Index of Consumer Sentiment
 #' 
 #' University of Michigan Consumer Survey, Index of Consumer Sentiment: 1980 - 2017.
-#' 
+#' Units: Index 1966:Q1=100
 #' 
 #' @format Time series data - 'xts' object
 #' @source University of Michigan, University of Michigan: Consumer Sentiment
@@ -60,7 +61,7 @@
 #' Crude Oil Prices: Brent - Europe
 #' 
 #' Crude Oil Prices: Brent - Europe: 1987 - 2017.
-#' 
+#' Units: Dollars per Barrel
 #' 
 #' @format Time series data - 'zoo'
 #' @source U.S. Energy Information Administration, Crude Oil Prices: Brent - Europe [MCOILBRENTEU], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/MCOILBRENTEU, January 8, 2018.
