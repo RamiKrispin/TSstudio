ac12d3cc1681f2f0cdb68f8e8c8f9963a93e7a48
RamiKrispin <ramkrisp@umich.edu>
2018-09-20 20:51:51 -0700

    Resizing the vignette size

--- inst/doc/Forecasting_Tools.R
+++ inst/doc/Forecasting_Tools.R
@@ -6,7 +6,7 @@ knitr::opts_chunk$set(echo = TRUE)
 library(TSstudio)
 packageVersion("TSstudio")
 
-## ----fig.height=5, fig.width=10------------------------------------------
+## ----fig.height=4, fig.width=7-------------------------------------------
 # Loading the data
 data("USgas", package = "TSstudio")
 
@@ -26,7 +26,7 @@ test <- USgas_splits$test
 ts_info(train)
 ts_info(test)
 
-## ----fig.height=5, fig.width=10------------------------------------------
+## ----fig.height=4, fig.width=7-------------------------------------------
 library(forecast)
 
 # Setting the forecasting horizon to the length of the testing partition
@@ -37,32 +37,32 @@ fc1 <- forecast(md1, h = h)
 
 test_forecast(actual = USgas, forecast.obj = fc1, test = test)
 
-## ----fig.height=5, fig.width=10------------------------------------------
+## ----fig.height=4, fig.width=7-------------------------------------------
 md2 <- auto.arima(USgas)
 fc2 <- forecast(md2, h = 60)
 
 plot_forecast(fc2)
 
-## ----fig.height=5, fig.width=10------------------------------------------
+## ----fig.height=4, fig.width=7-------------------------------------------
 check_res(md2)
 
-## ----message=FALSE, warning=FALSE, fig.height=5, fig.width=10------------
+## ----message=FALSE, warning=FALSE, fig.height=4, fig.width=7-------------
 USgas_backtesting <- ts_backtesting(ts.obj = USgas,
-                                    models = "abehntw",
+                                    models = "aentw",
                                     periods = 6,
                                     error = "MAPE",
                                     window_size = 12,
                                     h = 60, 
                                     plot = FALSE)
 
-## ---- fig.height=10, fig.width=10----------------------------------------
+## ---- fig.height=6, fig.width=7------------------------------------------
 USgas_backtesting$summary_plot
 
 ## ------------------------------------------------------------------------
 USgas_backtesting$leaderboard
 
-## ---- fig.height=5, fig.width=10-----------------------------------------
+## ---- fig.height=4, fig.width=7------------------------------------------
 plot_forecast(USgas_backtesting$Forecast_Final$nnetar)
-plot_forecast(USgas_backtesting$Forecast_Final$bsts)
-plot_forecast(USgas_backtesting$Forecast_Final$hybrid)
+plot_forecast(USgas_backtesting$Forecast_Final$auto.arima)
+plot_forecast(USgas_backtesting$Forecast_Final$tbats)
 
